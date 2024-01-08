22104787856ac303e433c7431940d01c2556d00e
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-01-08 00:21:18 -0300

    feat: create formmat utils and use it

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -7,6 +7,7 @@ import {
   TransactionsTable,
 } from './styles'
 import { useTransaction } from '../../hooks/useTransaction'
+import { dateFormatter, priceFormatter } from '../../utils/formmater'
 
 export function TransactionsPage() {
   const { transactions } = useTransaction()
@@ -26,11 +27,14 @@ export function TransactionsPage() {
                   <td width="50%">{transaction.description}</td>
                   <td>
                     <PriceHighlight variant={transaction.type}>
-                      {transaction.price}
+                      {transaction.type === 'outcome' && '- '}
+                      {priceFormatter.format(transaction.price)}
                     </PriceHighlight>
                   </td>
                   <td>{transaction.category}</td>
-                  <td>{transaction.createdAt}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
                 </tr>
               )
             })}
